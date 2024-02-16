5259d5473754a0172a0fc5aef6f90d12b72c0fb3
belkhalil <belkhalil1mohamed@gmail.com>
2024-02-16 17:50:07 +0100

    building JS file and few modifications in html and css files

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const input = document.getElementById('birth');
+const button = document.getElementById('btn');
+const result = document.getElementById('result');
+
+ 
+
+
+function ageCalculator(e) {
+
+e.preventDefault();
+
+    const birthdayValue = input.value
+
+    if ( birthdayValue === '' ) {
+        alert('Please Enter your date of birth')
+    } else { 
+
+        const age = getAge(birthdayValue);
+        result.innerText = `you age is ${age}  ${age > 1 ?'years' : 'year'} old`;
+
+    }
+
+    function getAge(birthdayValue) {
+
+        const today = new Date();
+        const birthDate = new Date(birthdayValue)
+
+     let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+    const day = today.getDate() - birthDate.getDate();
+
+          if ( month < 0 || (month === '0' && day <= '0')) {
+            age--;
+          }
+
+    return age;
+
+    }
+
+}
+
+button.addEventListener('click', ageCalculator) 
+
